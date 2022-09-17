641d89e08d1b53f460417c64ac95177d847c1ffe
André Sousa <andre.lipe.o@gmail.com>
2022-09-17 13:04:54 -0300

    feat: adiciona classe Livro

---
+++ atividade05/src/main/java/atividade05/model/Livro.java
@@ -0,0 +1,46 @@
+package atividade05.model;
+
+import java.time.Year;
+import java.util.Collection;
+
+public class Livro {
+
+	private String titulo;
+	@SuppressWarnings("unused")
+	private String subtitulo;
+	@SuppressWarnings("unused")
+	private String tituloOriginal;
+	@SuppressWarnings("unused")
+	private String subtituloOriginal;
+	private Collection<String> autores;
+	@SuppressWarnings("unused")
+	private Collection<String> tradutores;
+	private String idiomaOriginal;
+	@SuppressWarnings("unused")
+	private String idiomaDaTraducao;
+	private String editora;
+	@SuppressWarnings("unused")
+	private Year anoDaObra;
+	private Year anoDaEdicao;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public Collection<String> getAutores() {
+		return autores;
+	}
+
+	public String getIdiomaOriginal() {
+		return idiomaOriginal;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public Year getAnoDaEdicao() {
+		return anoDaEdicao;
+	}
+
+}
