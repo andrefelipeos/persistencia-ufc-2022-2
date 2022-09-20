eaa96fabfd9c5aa45bd08e8190f43b0569496cc8
André Sousa <andre.lipe.o@gmail.com>
2022-09-19 21:30:24 -0300

    feat: adiciona classe-entidade Álbum

---
+++ trabalho01/src/main/java/trabalho01/model/Album.java
@@ -0,0 +1,31 @@
+package trabalho01.model;
+
+public class Album {
+
+	private String titulo;
+	private String artista;
+	private String genero;
+	private String gravadora;
+	private int anoDeLancamento;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public String getGravadora() {
+		return gravadora;
+	}
+
+	public int getAnoDeLancamento() {
+		return anoDeLancamento;
+	}
+
+}
