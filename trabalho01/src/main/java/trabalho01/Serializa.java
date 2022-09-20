599f23ecab027d40dfd177ee44a668141a734076
André Sousa <andre.lipe.o@gmail.com>
2022-09-20 06:28:28 -0300

    feat: extrai código para dois métodos na classe Serializa
    Cria dois novos métodos: 'salvaComoJSON' e 'defineAlbumPeloTeclado'

--- trabalho01/src/main/java/trabalho01/Serializa.java
+++ trabalho01/src/main/java/trabalho01/Serializa.java
@@ -1,7 +1,10 @@
 package trabalho01;
 
 import java.io.File;
+import java.io.IOException;
 
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -12,16 +15,26 @@ public class Serializa {
 	public static void main(String[] args) throws Exception {
 		String localDoArquivo = "album.json";
 
-		Album album = new Album();
-		album.setTitulo("DawnFM");
-		album.setArtista("The Weeknd");
-		album.setGenero("Dance-pop, synth-pop");
-		album.setGravadora("XO e Republic Records");
-		album.setAnoDeLancamento(2022);
+		Album album = Serializa.defineAlbumPeloTeclado();
 
+		Serializa.salvaComoJSON(localDoArquivo, album);
+	}
+
+	private static void salvaComoJSON(String localDoArquivo, Album album)
+			throws IOException, StreamWriteException, DatabindException {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.enable(SerializationFeature.INDENT_OUTPUT);
 		mapper.writeValue(new File(localDoArquivo), album);
+	}
+
+	private static Album defineAlbumPeloTeclado() {
+		Album novoAlbum = new Album();
+		novoAlbum.setTitulo("DawnFM");
+		novoAlbum.setArtista("The Weeknd");
+		novoAlbum.setGenero("Dance-pop, synth-pop");
+		novoAlbum.setGravadora("XO e Republic Records");
+		novoAlbum.setAnoDeLancamento(2022);
+		return novoAlbum;
 	}
 
 }
