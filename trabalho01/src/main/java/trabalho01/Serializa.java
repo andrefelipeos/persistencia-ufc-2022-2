d20296acc79f86893d0621195a0e6bc24944e96d
André Sousa <andre.lipe.o@gmail.com>
2022-09-23 18:42:01 -0300

    finaliza questão 2 ao receber dados do teclado na classe serializa

--- trabalho01/src/main/java/trabalho01/Serializa.java
+++ trabalho01/src/main/java/trabalho01/Serializa.java
@@ -1,7 +1,14 @@
+/*
+ * 2. Crie uma classe Java para cadastrar dados relacionados à entidade
+ * definida na questão 1. A classe deve receber dados via teclado e os salvar
+ *  em um arquivo JSON.
+ * */
+
 package trabalho01;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.core.exc.StreamWriteException;
 import com.fasterxml.jackson.databind.DatabindException;
@@ -28,6 +35,39 @@ public class Serializa {
 	}
 
 	private static Album defineAlbumPeloTeclado() {
+		Scanner teclado = new Scanner(System.in);
+
+		String titulo;
+		String artista;
+		String genero;
+		String gravadora;
+		int anoDeLancamento;
+
+		System.out.print("Título: ");
+		titulo = teclado.nextLine();
+		System.out.print("Artista: ");
+		artista = teclado.nextLine();
+		System.out.print("Gênero: ");
+		genero = teclado.nextLine();
+		System.out.print("Gravadora: ");
+		gravadora = teclado.nextLine();
+		System.out.print("Ano de lançamento: ");
+		anoDeLancamento = teclado.nextInt();
+
+		Album novoAlbum = new Album();
+		novoAlbum.setTitulo(titulo);
+		novoAlbum.setArtista(artista);
+		novoAlbum.setGenero(genero);
+		novoAlbum.setGravadora(gravadora);
+		novoAlbum.setAnoDeLancamento(anoDeLancamento);
+
+		teclado.close();
+
+		return novoAlbum;
+	}
+
+	@SuppressWarnings("unused")
+	private static Album defineAlbumSemInteracao() {
 		Album novoAlbum = new Album();
 		novoAlbum.setTitulo("DawnFM");
 		novoAlbum.setArtista("The Weeknd");
