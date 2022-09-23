c7989afb7f041f2e92217facd1d0546161e4e98e
André Sousa <andre.lipe.o@gmail.com>
2022-09-23 19:38:01 -0300

    converte JSON para XML

--- trabalho01/src/main/java/trabalho01/Conversor.java
+++ trabalho01/src/main/java/trabalho01/Conversor.java
@@ -5,9 +5,18 @@
 
 package trabalho01;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 public class Conversor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String localDoArquivo;
 
 		if (args.length == 0) {
@@ -16,6 +25,15 @@ public class Conversor {
 		} else {
 			localDoArquivo = args[0];
 		}
+		
+		String json = new String(Files.readAllBytes(Paths.get(localDoArquivo)));
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode jsonNode = mapper.readTree(json);
+		
+		ObjectMapper xmlMapper = new XmlMapper();
+		xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String xml = xmlMapper.writeValueAsString(jsonNode);
+		System.out.println(xml);
 	}
 
 }
