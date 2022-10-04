6ff88bd86cdabe8e471652261e55595835641cd2
André Sousa <andre.lipe.o@gmail.com>
2022-10-04 20:53:49 -0300

    feat: adiciona método que define um aluno por entradas do teclado

--- atividade06/src/main/java/atividade06/Controlador.java
+++ atividade06/src/main/java/atividade06/Controlador.java
@@ -3,6 +3,9 @@ package atividade06;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
+
+import atividade06.modelos.Aluno;
 
 public class Controlador {
 
@@ -12,6 +15,32 @@ public class Controlador {
 				"postgres", "postgres");
 		System.out.println("Conectado!");
 		conexao.close();
+	}
+
+	private static Aluno defineAlunoPeloTeclado(Scanner entrada) {
+		int id;
+		String cpf;
+		int matricula;
+		String nome;
+		String email;
+		String telefone;
+
+		System.out.print("Identificador: ");
+		id = entrada.nextInt();
+		System.out.print("CPF: ");
+		cpf = entrada.nextLine();
+		System.out.print("Matrícula: ");
+		matricula = entrada.nextInt();
+		System.out.print("Nome: ");
+		nome = entrada.nextLine();
+		System.out.print("E-mail: ");
+		email = entrada.nextLine();
+		System.out.print("Telefone: ");
+		telefone = entrada.nextLine();
+
+		Aluno novoAluno = new Aluno(id, cpf, matricula, nome, email, telefone);
+
+		return novoAluno;
 	}
 
 }
