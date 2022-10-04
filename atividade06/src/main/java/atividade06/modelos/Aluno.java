c939e04503ee952dda5ca3ff02bcad1f7406bb33
André Sousa <andre.lipe.o@gmail.com>
2022-10-04 20:50:02 -0300

    feat: adiciona classe modelo 'Aluno'

---
+++ atividade06/src/main/java/atividade06/modelos/Aluno.java
@@ -0,0 +1,47 @@
+package atividade06.modelos;
+
+public class Aluno {
+
+	private int id;
+	private String cpf;
+	private int matricula;
+	private String nome;
+	private String email;
+	private String telefone;
+
+	public Aluno(int id, String cpf, int matricula,
+			String nome, String email, String telefone) {
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.matricula = matricula;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+}
