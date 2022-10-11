3274bd2a6e16f6d56d6235680fcfbe920f94f355
André Sousa <andre.lipe.o@gmail.com>
2022-10-11 11:39:04 -0300

    refactor: acessa o banco usando uma connection factory e um arquivo de propriedades

--- atividade07/src/main/java/atividade07/Principal.java
+++ atividade07/src/main/java/atividade07/Principal.java
@@ -1,21 +1,20 @@
 package atividade07;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import atividade07.dao.ConnectionFactory;
+
 public class Principal {
 
 	public static void main(String[] args) throws SQLException {
-		Connection conexao = DriverManager
-				.getConnection("jdbc:postgresql://localhost/persistencia",
-						"postgres", "postgres");
+		Connection conexao = ConnectionFactory.getConnection();
 		PreparedStatement ps = conexao.prepareStatement("SELECT * FROM alunos;");
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
-			System.out.println(rs.getString("nome") + " : " + rs.getString("matricula"));
+			System.out.println(rs.getString("matricula") + " : " + rs.getString("nome"));
 		}
 		conexao.close();
 	}
