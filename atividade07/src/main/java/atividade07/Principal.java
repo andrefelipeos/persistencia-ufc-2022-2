a511229dd54f4eea913e7ba73f3300123916c06e
André Sousa <andre.lipe.o@gmail.com>
2022-10-11 23:50:01 -0300

    feat: adiciona menu de opções na atividade 07

--- atividade07/src/main/java/atividade07/Principal.java
+++ atividade07/src/main/java/atividade07/Principal.java
@@ -2,6 +2,7 @@ package atividade07;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 import atividade07.dao.AlunoDAO;
 import atividade07.dao.AlunoDAOImpl;
@@ -10,11 +11,59 @@ import atividade07.modelos.Aluno;
 public class Principal {
 
 	public static void main(String[] args) throws SQLException {
+		Scanner teclado = new Scanner(System.in);
+
 		AlunoDAO alunoDAO = new AlunoDAOImpl();
-		List<Aluno> listaDeAlunos = alunoDAO.findAll();
-		for (Aluno aluno : listaDeAlunos) {
-			System.out.println(aluno);
+
+		String opcao;
+		while (true) {
+			System.out.println("Opções: 'cadastrar', 'atualizar', 'apagar' e 'listar'.");
+			System.out.print(" > ");
+			opcao = teclado.nextLine();
+			if (opcao.equals("cadastrar")) {
+				Aluno aluno = Principal.defineAlunoPeloTeclado(teclado);
+				alunoDAO.save(aluno);
+			} else if (opcao.equals("atualizar")) {
+				System.out.print("Identificador: ");
+				int identificador = Integer.parseInt(teclado.nextLine());
+				Aluno aluno = Principal.defineAlunoPeloTeclado(teclado);
+				aluno.setIdentificador(identificador);
+				alunoDAO.save(aluno);
+			} else if (opcao.equals("apagar")) {
+				Integer matricula = Integer.valueOf(teclado.nextLine());
+				alunoDAO.deleteByMatricula(matricula);
+			} else if (opcao.equals("listar")) {
+				List<Aluno> listaDeAlunos = alunoDAO.findAll();
+				for (Aluno aluno : listaDeAlunos) {
+					System.out.println(aluno);
+				}
+			} else {
+				break;
+			}
 		}
+	}
+
+	private static Aluno defineAlunoPeloTeclado(Scanner entrada) {
+		String cpf;
+		int matricula;
+		String nome;
+		String email;
+		String telefone;
+
+		System.out.print("CPF: ");
+		cpf = entrada.nextLine();
+		System.out.print("Matrícula: ");
+		matricula = Integer.parseInt(entrada.nextLine());;
+		System.out.print("Nome: ");
+		nome = entrada.nextLine();
+		System.out.print("E-mail: ");
+		email = entrada.nextLine();
+		System.out.print("Telefone: ");
+		telefone = entrada.nextLine();
+
+		Aluno novoAluno = new Aluno(matricula, nome, cpf, email, telefone);
+
+		return novoAluno;
 	}
 
 }
