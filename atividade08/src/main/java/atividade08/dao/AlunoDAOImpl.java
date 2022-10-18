c08c27125d3094ea09b09e28edd4a8422b11e2bc
André Sousa <andre.lipe.o@gmail.com>
2022-10-18 11:29:57 -0300

    refactor: remove imports não usados

--- atividade08/src/main/java/atividade08/dao/AlunoDAOImpl.java
+++ atividade08/src/main/java/atividade08/dao/AlunoDAOImpl.java
@@ -1,9 +1,5 @@
 package atividade08.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
