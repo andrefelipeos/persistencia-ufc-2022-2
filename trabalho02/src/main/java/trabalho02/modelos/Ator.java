b90f1bbba480984515f3223248de87ada750167e
André Sousa <andre.lipe.o@gmail.com>
2022-11-10 16:50:27 -0300

    feat: creates model 'Ator' as a entity

---
+++ trabalho02/src/main/java/trabalho02/modelos/Ator.java
@@ -0,0 +1,31 @@
+package trabalho02.modelos;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "atores")
+public class Ator {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer identificador;
+	private String nome;
+	private LocalDate dataDeNascimento;
+
+	public Ator(String nome, LocalDate dataDeNascimento) {
+		this.nome = nome;
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	@Override
+	public String toString() {
+		return nome + ", nascido em " + dataDeNascimento.toString();
+	}
+
+}
