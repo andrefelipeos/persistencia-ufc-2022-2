34c3bace586f6be44c1d9a4ad6bcbe25c3a203c2
André Sousa <andre.lipe.o@gmail.com>
2022-11-10 15:05:44 -0300

    feat: creates model 'Ator' as a JPA entity

---
+++ trabalho02/src/main/java/trabalho02/modelos/Filme.java
@@ -0,0 +1,31 @@
+package trabalho02.modelos;
+
+import java.time.Year;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "filmes")
+public class Filme {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idenficador;
+	private String titulo;
+	private Year anoDeLancamento;
+
+	public Filme(String titulo, int anoDeLancamento) {
+		this.titulo = titulo;
+		this.anoDeLancamento = Year.of(anoDeLancamento);
+	}
+
+	@Override
+	public String toString() {
+		return titulo + " (" + anoDeLancamento.toString() + ")";
+	}
+
+}
